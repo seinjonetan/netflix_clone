bfbdf965e87f71a06b6786a2f0fb57d3c2d3b714
Tan Sein Jone <seinjonetan@gmail.com>
2023-04-21 16:43:12 +0800

    feat: added modals for movies
    -  useInfoModal.ts handles the state of the modal
    (movie seletced, opening and closing, visible state)
    - InfoModal.tsx is the component that shows up when more info clicked
    - info modal contains the video, title, duration, genre, description
    - fully functional play, favorite buttons
    - added modal trigger to billboard and movie cards

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,10 +1,16 @@
 import useBillboard from "@/hooks/useBillboard";
-import React from "react";
+import React, { useCallback } from "react";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import PlayButton from "./PlayButton";
+import useInfoModal from "@/hooks/useInfoModal";
 
 const Billboard = () => {
   const { data } = useBillboard();
+  const { openModal } = useInfoModal();
+
+  const handleOpenModal = useCallback(() => {
+    openModal(data?.id);
+  }, [openModal, data?.id]);
 
   return (
     <div className="relative h-[56.25vw]">
@@ -73,6 +79,7 @@ const Billboard = () => {
           items-center
           hover:bg-opacity-20
           transition"
+            onClick={handleOpenModal}
           >
             <AiOutlineInfoCircle className="mr-1" />
             More Info
